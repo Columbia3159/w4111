783be1c0b270e698b5f42b8276e1fb3f9dabc5a5
Khaliun G. <khaliun0122@gmail.com>
2024-11-18 16:19:36 -0500

    Revert "feat: login screen and layout (#1)"
    This reverts commit c7ebaa145ec3e6bb84413de4269e8478713edbb3.

--- webserver/server.py
+++ webserver/server.py
@@ -1,14 +1,11 @@
 import os
-from flask import Flask, g, render_template
+from flask import Flask, g
 
 from db import *
 from routes.public import public_bp
-from routes.auth import auth_bp
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-
-app.secret_key = os.urandom(24)
 
 @app.before_request
 def before_request():
@@ -19,13 +16,7 @@ def teardown_request(exception):
   close_db()
 
 
-@app.errorhandler(401)
-def not_authorized(error):
-  return render_template("page_401.html"), 401
-
-
 # Register Blueprints
-app.register_blueprint(auth_bp)
 app.register_blueprint(public_bp)
 
 
